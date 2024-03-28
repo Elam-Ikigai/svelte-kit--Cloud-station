652b6ed0885998aa5d765bb7353069067ee7edf4
Elam-Ikigai <142744822+Elam-Ikigai@users.noreply.github.com>
2024-03-28 17:53:51 +0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,7 @@
 	</h1>
 
 	<h2>
-		try editing <strong>Env2/test today 28th March 2024</strong>
+		try editing <strong>Env2/test today 28th March 5000 aliens date </strong>
 	</h2>
 
 	<Counter />
